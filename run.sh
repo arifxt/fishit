ff9d4de3e23bce5ac2074429cb8da955fdbcb930
arif <arifxtry@gmail.com>
2026-02-22 06:42:51 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ timeout 10m bash -c '
 while true
 do
   cp -u *ch.json ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Meng\ Hub_FishIt/Config/.
-  cp -u cp *m.json ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Chloe\ X/FishIt/.
+  cp -u *m.json ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Chloe\ X/FishIt/.
   sleep 5
 done
 '
