6ed239fb855d25e119d5ff8ebe655338d8fd6e9a
arif <arifxtry@gmail.com>
2026-02-22 06:33:58 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,13 +8,15 @@ wget https://raw.githubusercontent.com/arifxt/fishit/refs/heads/main/elementm.js
 #Gospin Menghub
 wget https://raw.githubusercontent.com/arifxt/fishit/refs/heads/main/gospinm.json
 
-#mkdir -p  ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Chloe\ X/FishIt/
-#mkdir -p ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Meng\ Hub_FishIt/Config/
+mkdir -p  ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Chloe\ X/FishIt/
+mkdir -p ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Meng\ Hub_FishIt/Config/
 
-sleep 3
-for i in {1..500}
+#Time !0 menit
+end=$((SECONDS+600))
+
+while [ $SECONDS -lt $end ]
 do
-  mv *m.json ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Chloe\ X/FishIt/.
- mv *ch.json ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Meng\ Hub_FishIt/Config/.
+  cp -u *ch.json ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Meng\ Hub_FishIt/Config/.
+  cp -u cp *m.json ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Chloe\ X/FishIt/.
+  sleep 5
 done
-
