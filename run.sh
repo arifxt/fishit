00d1bc87af501d7759322ac2c9281d65438084ea
arif <arifxtry@gmail.com>
2026-02-22 06:35:16 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,12 +11,12 @@ wget https://raw.githubusercontent.com/arifxt/fishit/refs/heads/main/gospinm.jso
 mkdir -p  ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Chloe\ X/FishIt/
 mkdir -p ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Meng\ Hub_FishIt/Config/
 
-#Time !0 menit
-end=$((SECONDS+600))
 
-while [ $SECONDS -lt $end ]
+timeout 10m bash -c '
+while true
 do
   cp -u *ch.json ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Meng\ Hub_FishIt/Config/.
   cp -u cp *m.json ~/storage/shared/Android/data/com.roblox.client/files/gloop/external/Workspace/Chloe\ X/FishIt/.
   sleep 5
 done
+'
